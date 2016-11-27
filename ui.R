51e6148784ce95c0740e1964d22a494ffafb2178
Alex Vollmer <alex.vollmer@gmail.com>
2016-11-27 08:57:27 -0800

    Improved explanatory text in Shiny app UI.

--- ui.R
+++ ui.R
@@ -9,14 +9,18 @@ shinyUI(fluidPage(
   sidebarLayout(
     sidebarPanel(
       h3("Graph Configuration"),
-      p("Select a variable from the mtcars dataset for each aesthetic dimesion of the graph"),
+      p("Select a variable from the mtcars dataset for each aesthetic ",
+        "dimesion of the graph. Once all five aesthetics have been chosen, ",
+        "the plot of the data will be rendered accordingly"),
       uiOutput("error"),
       uiOutput("warning"),
       uiOutput("xaxis"),
       uiOutput("yaxis"),
       uiOutput("color"),
       uiOutput("shape"),
-      uiOutput("size")
+      uiOutput("size"),
+      p("You can view the source code for this project on ",
+        a("GitHub", href="https://github.com/alexvollmer/data-products-week4"))
     ),
 
     # Show a plot of the generated distribution
